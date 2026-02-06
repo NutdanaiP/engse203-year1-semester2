b8ef64123679d79808bb97a2ad4d0a5daf56c989
NutdanaiP <nutdanai@live.rmutl.ac.th>
2026-02-06 10:51:13 +0700

    Add product routes with validation

--- week2-day3/Workshop-11-Level1_Product-Management-API/level-1-guided/product-api/src/routes/products.js
+++ week2-day3/Workshop-11-Level1_Product-Management-API/level-1-guided/product-api/src/routes/products.js
@@ -1,4 +1,4 @@
-// src/routes/products.js
+// สร้างไฟล์ src/routes/products.js
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
